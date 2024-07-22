151c852c4ef08cc017cdf04a7954bfec7bf4c55d
jesse318s <jesse318s@gmail.com>
2024-07-22 10:48:25 -0500

    Add project files.

---
+++ lab-2-csharp/frmMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lab_2_csharp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
